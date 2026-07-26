82dc30f7937050d33c0e40b01e6696e75a4f24e1
252t605510-sudo <252t605510@ashokacollege.in>
2026-07-26 23:31:59 +0530

    add PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,19 @@
+public class PrimitiveDataTypes{
+
+	public static void main(String[] args) {
+	    int rollNo=64000;//4-byte
+	    short s=1000;//2-byte
+	    byte b=10;//1byte
+	    char letter ='a';//2-byte
+	    float marks=89.7f;//4-byte
+	    double LargeDecimalNumber=4567778.987;//8-byte
+	    long LongInteger=324567891234l;//8-byte
+	    boolean check =false;
+	    System.out.println(rollNo);
+	    System.out.println(letter);
+	    System.out.println(marks);
+	    System.out.println(LargeDecimalNumber);
+	    System.out.println(LongInteger);
+	    System.out.println(check);
+	}
+}
