50f7ac5c88869f35807865215997769b2adaf794
252t605510-sudo <252t605510@ashokacollege.in>
2026-07-27 09:33:32 +0530

    ImplicitTyepeCasting.java file added

---
+++ ImplicitTyepeCasting.java
@@ -0,0 +1,14 @@
+public class ImplicitTyepeCasting
+{
+	public static void main(String[] args) {
+	    int i=100;
+	    long l=i;
+	    float f =l;
+	    double d=f;
+	    
+		 System.out.println("int value:"+i);
+		 System.out.println("long value:"+l);
+		 System.out.println("float value:"+f);
+		 System.out.println("double value:"+d);
+	}
+}
